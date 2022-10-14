7ffc36883df7d04566ce9df3da40b489fc7de358
Kócsó Balázs <kocsob@balabit.hu>
2022-10-14 20:16:53 +0000

    Import Upstream version 0.3.4

---
+++ test.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*-
+
+import datetime
+
+import leather
+
+# data1 = [
+#     (2, 'foo'),
+#     (6, 'bar'),
+#     (9, 'bing')
+# ]
+#
+# data2 = [
+#     (3, 'foo'),
+#     (5, 'bar'),
+#     (7, 'bing')
+# ]
+#
+# lattice = leather.Lattice(shape=leather.Bars())
+# lattice.add_many([data1, data2])
+# lattice.to_svg('test.svg')
+
+data1 = [
+    (2, None),
+    (3, None)
+]
+
+chart = leather.Chart()
+chart.add_bars(data1)
+chart.to_svg('test.svg')
